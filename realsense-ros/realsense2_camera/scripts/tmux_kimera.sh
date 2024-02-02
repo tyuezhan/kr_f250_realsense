ad1f26ed984f0dd3def82b327c7bfac5dc3346f9
Yuezhan Tao <yztao@seas.upenn.edu>
2024-02-02 12:21:58 -0500

    Varun and Yuezhan: launch file for Jackal d455

--- realsense-ros/realsense2_camera/scripts/tmux_kimera.sh
+++ realsense-ros/realsense2_camera/scripts/tmux_kimera.sh
@@ -4,7 +4,7 @@ WORLD_FRAME_ID=world
 
 # MASTER_URI=http://10.42.0.1:11311
 # SETUP_ROS_STRING="export ROS_MASTER_URI=${MASTER_URI}"
-SETUP_ROS_STRING=""
+# SETUP_ROS_STRING=" "
 SESSION_NAME=kimera
 
 CURRENT_DISPLAY=${DISPLAY}
@@ -27,13 +27,13 @@ fi
 tmux setw -g mouse on
 
 tmux new-window -t $SESSION_NAME -n "Main"
-tmux send-keys -t $SESSION_NAME "$SETUP_ROS_STRING; sleep 1; roslaunch realsense2_camera d455.launch" Enter
+tmux send-keys -t $SESSION_NAME "sleep 1; roslaunch realsense2_camera d455.launch" Enter
 tmux select-layout -t $SESSION_NAME tiled
 
 tmux new-window -t $SESSION_NAME -n "Kimera"
-tmux send-keys -t $SESSION_NAME "$SETUP_ROS_STRING; sleep 2; roslaunch kimera_vio_ros kimera_vio_ros_realsense_IR.launch" Enter
-# tmux split-window -t $SESSION_NAME
-# tmux send-keys -t $SESSION_NAME "$SETUP_ROS_STRING; sleep 4; roslaunch scan2shape_launch real_robot_process_cloud_node.launch" Enter
+tmux send-keys -t $SESSION_NAME "sleep 2; roslaunch kimera_vio_ros kimera_vio_ros_realsense_IR.launch" Enter
+tmux split-window -t $SESSION_NAME
+tmux send-keys -t $SESSION_NAME "rviz -d $(rospack find kimera_vio_ros)/rviz/kimera_vio_euroc.rviz" Enter
 
 
 # Add window to easily kill all processes
